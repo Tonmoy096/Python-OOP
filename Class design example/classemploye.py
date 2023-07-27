96c7ccacf1dced4c930dca99e49ef784b62eac8b
Tauntmoy <foysalkabir2017@gmail.com>
2023-07-28 01:07:24 +0600

    oop

---
+++ Class design example/classemploye.py
@@ -0,0 +1,14 @@
+class Emplopyee:
+    #parameterized constructor
+    def __init__(self,name,no):
+        self.no=no  #instance variable
+        self.name=name  #instance variable
+    
+    #instance method
+    def display(self):
+        print(self.name,self.no)
+
+emp1=Emplopyee("John",11)  #instance 1
+emp2=Emplopyee("David",12)  #instance 2
+emp1.display()
+emp2.display()
