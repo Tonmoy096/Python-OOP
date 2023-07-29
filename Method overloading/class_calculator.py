65f8f282d5d76366c6cc53794d69ad57d9ef1065
Tauntmoy <foysalkabir2017@gmail.com>
2023-07-30 02:00:24 +0600

    oop

--- Method overloading/class_calculator.py
+++ Method overloading/class_calculator.py
@@ -1,10 +1,10 @@
 class Calculator:
 
     def product(self,*num):
-        sum=1
+        prod=1
         for x in num:
-            sum=sum*x
-        print(sum)
+            prod=prod*x
+        print(prod)
 
 c1=Calculator()
 c1.product(4,5,6,7)
