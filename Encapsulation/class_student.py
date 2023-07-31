73a482bf020bc6b093e19acc1aabdf17ed3536c6
Tauntmoy <foysalkabir2017@gmail.com>
2023-08-01 02:30:40 +0600

    oop

---
+++ Encapsulation/class_student.py
@@ -0,0 +1,22 @@
+class Student:
+    def __init__(self, name, id):
+        self.name = name  # instance variable
+        self.__id = id  # private instance variable
+
+    def view(self):
+        print(self.name, self.__id)
+
+    def update_id(self, id):
+        if id > 0:
+            self.__id = id
+        else:
+            print("invalid id, enter id again")
+
+
+s1 = Student("Bob", 1240144)
+s2 = Student("Lala", 7896)
+
+s1.update_id(10)
+
+s1.view()
+s2.view()
